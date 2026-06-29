4a50fa29b67a54af453b1bd837d92c395cb01fb1
unknown <gdelmelo2santos@gmail.com>
2026-06-30 00:24:55 +0200

    Revert "Seguimiento e idioma"
    This reverts commit ab2dea006be7a8b8af54d4ef88d20a075a1ff89a.

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "192.168.1.163"],
-};
-
-export default nextConfig;
--- src/lib/i18n-server.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers'
-import { defaultLocale, isLocale, localeCookieName, type Locale } from './i18n'
-
-export async function getLocale(): Promise<Locale> {
-  const cookieStore = await cookies()
-  const locale = cookieStore.get(localeCookieName)?.value
-
-  return isLocale(locale) ? locale : defaultLocale
-}
